ff0aca6b84fc81c6cd30b2ab165e1dbd2b165c9d
Robby Hunt <robbyshunt@gmail.com>
2020-10-19 20:04:29 +0100

    deploy react app to github

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './img/mandala.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Dreamspinner - Coming Soon
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
