530fe8098b0a6b10aef1710a84f8948d81b9e2db
Robby Hunt <robby@Robbys-MacBook-Pro.local>
2020-12-11 00:47:13 +0000

    added generator

--- src/Components/Play.js
+++ src/Components/Play.js
@@ -1,33 +1,65 @@
 import React from 'react';
 import Styled from "@emotion/styled";
+import Generator from '../Generators/Generator';
 
 const Wrapper = Styled('div')`
-  background-color: #eaeaea;
-  height: calc(100vh - 270px);
+  background-color: #00467f;
+  background-image: url(http://www.transparenttextures.com/patterns/light-paper-fibers.png);
+  height: calc(100vh - 130px);
   width: 100vw;
-  padding: 90px 0;
+  padding: 20px 0;
   position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media (min-width: 820px) {
+    padding: 90px 0;
+    height: calc(100vh - 270px);
+  }
+`;
+const ButtonWrapper = Styled('div')`
+  display: flex;
+  justify-content: space-between;
 `;
 
-const ButtonWrapper = Styled('div')`
-  width: 80.5vw;
+const GeneratorWrapper = Styled('div')`
   display: flex;
+  width: 40.25vw;
+`;
+
+const LogButtonWrapper = Styled('div')`
+  display: flex;
+  width: 40.25vw;
   justify-content: flex-end;
 `;
 
-const Clear = Styled('button')`
+const Button = Styled('button')`
   cursor: pointer;
   margin-left: 10px;
 `;
 
+const GenerateButton = Styled(Button)`
+  background-color: #000000;
+  color: #ffffff;
+  margin-left: 0px;
+  margin-right: 10px;
+`;
+
 const Log = Styled('textarea')`
-  width: 80vw;
+  width: calc(80vw - 10px);
   height: 100%;
   resize: none;
+  padding: 10px;
   margin-bottom: 10px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  font-size: 18px;
+  line-height: 30px;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 const Input = Styled('textarea')`
@@ -35,6 +67,13 @@ const Input = Styled('textarea')`
   height: 40px;
   resize: none;
   margin-top: 10px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+  font-size: 16px;
+
+  &:focus {
+    outline: none;
+  }
 `;
 
 
@@ -56,6 +95,20 @@ class Play extends React.Component {
     }
   }
 
+  submitButton() {
+    let lineBreak = ""
+    const log = document.getElementById('log').value
+    const newString = document.getElementById('input').value
+
+    if (log !== "") {
+      lineBreak = '\n'
+    }
+
+    document.getElementById('log').value = log + lineBreak + newString;
+
+    document.getElementById('input').value = "";
+  }
+
   clear() {
     document.getElementById('log').value = ""
   }
@@ -64,17 +117,43 @@ class Play extends React.Component {
     document.getElementById('log').value = document.getElementById('log').value.replace(/\n.*$/, '');
   }
 
+  generateAction() {
+    let lineBreak = ""
+    if (document.getElementById('log').value !== "") {
+      lineBreak = '\n'
+    }
+
+    document.getElementById('log').value = document.getElementById('log').value + lineBreak + Generator("action");
+  }
+
+  generateDescription() {
+    let lineBreak = ""
+    if (document.getElementById('log').value !== "") {
+      lineBreak = '\n'
+    }
+
+    document.getElementById('log').value = document.getElementById('log').value + lineBreak + Generator("description");
+  }
+
   render() {
     return (
       <Wrapper>
         <Log name="log" id="log" readOnly/>
 
         <ButtonWrapper>
-          <Clear id="undo" onClick={this.undo}>Undo</Clear>
-          <Clear id="clear" onClick={this.clear}>Clear Log</Clear>
+          <GeneratorWrapper>
+            <GenerateButton id="cqa" onClick={this.generateAction}>CQ: Action</GenerateButton>
+            <GenerateButton id="cqd" onClick={this.generateDescription}>CQ: Description</GenerateButton>
+          </GeneratorWrapper>
+
+          <LogButtonWrapper>
+            <Button id="submit" onClick={this.submitButton}>Submit</Button>
+            <Button id="undo" onClick={this.undo}>Undo</Button>
+            <Button id="clear" onClick={this.clear}>Clear Log</Button>
+          </LogButtonWrapper>
         </ButtonWrapper>
 
-        <Input name="input" id="input" placeholder="Type something here and press Shift + Enter to add it to the log..." rows={1} onKeyUp={this.submit} onKeyUp={this.submit}/>
+        <Input name="input" id="input" placeholder="Type something here and press Shift + Enter or click Submit..." rows={1} onKeyUp={this.submit} onKeyUp={this.submit}/>
       </Wrapper>
     );
   }
---
+++ src/Generators/Generator.js
@@ -0,0 +1,28 @@
+const action1 = ["Attainment", "Starting", "Neglect", "Fight", "Recruit", "Triumph", "Violate", "Oppose", "Malice", "Communicate", "Persecute", "Increase", "Decrease", "Abandon", "Gratify", "Inquire", "Antagonize", "Move", "Waste", "Truce", "Release", "Befriend", "Judge", "Desert", "Dominate", "Procrastinate", "Praise", "Separate", "Take", "Break", "Heal", "Delay", "Stop", "Lie", "Return", "Imitate", "Struggle", "Inform", "Bestow", "Postpone", "Expose", "Haggle", "Imprison", "Release", "Celebrate", "Develop", "Travel", "Block", "Harm", "Debase", "Overindulge", "Adjourn", "Adversity", "Kill", "Disrupt", "Usurp", "Create", "Betray", "Agree", "Abuse", "Oppress", "Inspect", "Ambush", "Spy", "Attach", "Carry", "Open", "Carelessness", "Ruin", "Extravagance", "Trick", "Arrive", "Propose", "Divide", "Refuse", "Mistrust", "Deceive", "Cruelty", "Intolerance", "Trust", "Excitement", "Activity", "Assist", "Care", "Negligence", "Passion", "Work", "Control", "Attract", "Failure", "Pursue", "Vengeance", "Proceedings", "Dispute", "Punish", "Guide", "Transform", "Overthrow", "Oppress", "Change"]
+
+const action2 = ["Goals", "Dreams", "Environment", "Outside", "Inside", "Reality", "Allies", "Enemies", "Evil", "Good", "Emotions", "Opposition", "War", "Peace", "Innocent", "Love", "Spirit", "Intellect", "Ideas", "Joy", "Messages", "Energy", "Balance", "Tension", "Friendship", "Physical", "Project", "Pleasures", "Pain", "Possessions", "Benefits", "Plans", "Lies", "Expectations", "Legal", "Bureaucracy", "Business", "Path", "News", "Exterior", "Advice", "Plot", "Competition", "Prison", "Illness", "Food", "Attention", "Success", "Failure", "Travel", "Jealousy", "Dispute", "Home", "Investment", "Suffering", "Wishes", "Tactics", "Stalemate", "Randomness", "Misfortune", "Death", "Disruption", "Power", "Burden", "Intrigues", "Fears", "Ambush", "Rumor", "Wounds", "Extravagance", "Representative", "Adversities", "Opulence", "Liberty", "Military", "Mundane", "Trials", "Masses", "Vehicle", "Art", "Victory", "Dispute", "Riches", "Normal", "Technology", "Hope", "Magic", "Illusions", "Portals", "Danger", "Weapons", "Animals", "Weather", "Elements", "Nature", "Masses", "Leadership", "Fame", "Anger", "Information"]
+
+const description1 = ["Abnormally", "Adventurously", "Aggressively", "Angrily", "Anxiously", "Awkwardly", "Beautifully", "Bleakly", "Boldly", "Bravely", "Busily", "Calmly", "Carefully", "Carelessly", "Cautiously", "Ceaselessly", "Cheerfully", "Combatively", "Coolly", "Crazily", "Curiously", "Daintily", "Dangerously", "Defiantly", "Deliberately", "Delightfully", "Dimly", "Efficiently", "Energetically", "Enormously", "Enthusiastically", "Excitedly", "Fearfully", "Ferociously", "Fiercely", "Foolishly", "Fortunately", "Frantically", "Freely", "Frighteningly", "Fully", "Generously", "Gently", "Gladly", "Gracefully", "Gratefully", "Happily", "Hastily", "Healthily", "Helpfully", "Helplessly", "Hopelessly", "Innocently", "Intensely", "Interestingly", "Irritatingly", "Jovially", "Joyfully", "Judgementally", "Kindly", "Kookily", "Lazily", "Lightly", "Loosely", "Loudly", "Lovingly", "Loyally", "Majestically", "Meaningfully", "Mechanically", "Miserably", "Mockingly", "Mysteriously", "Naturally", "Neatly", "Nicely", "Oddly", "Offensively", "Officially", "Partially", "Peacefully", "Perfectly", "Playfully", "Politely", "Positively", "Powerfully", "Quaintly", "Quarrelsomely", "Quietly", "Roughly", "Rudely", "Ruthlessly", "Slowly", "Softly", "Swiftly", "Threateningly", "Very", "Violently", "Wildly", "Yieldingly"]
+
+const description2 = ["Abandoned", "Abnormal", "Amusing", "Ancient", "Aromatic", "Average", "Beautiful", "Bizarre", "Classy", "Clean", "Cold", "Colorful", "Creepy", "Cute", "Damaged", "Dark", "Defeated", "Delicate", "Delightful", "Dirty", "Disagreeable", "Disgusting", "Drab", "Dry", "Dull", "Empty", "Enormous", "Exotic", "Faded", "Familiar", "Fancy", "Fat", "Feeble", "Feminine", "Festive", "Flawless", "Fresh", "Full", "Glorious", "Good", "Graceful", "Hard", "Harsh", "Healthy", "Heavy", "Historical", "Horrible", "Important", "Interesting", "Juvenile", "Lacking", "Lame", "Large", "Lavish", "Lean", "Less", "Lethal", "Lonely", "Lovely", "Macabre", "Magnificent", "Masculine", "Mature", "Messy", "Mighty", "Military", "Modern", "Extravagant", "Mundane", "Mysterious", "Natural", "Nondescript", "Odd", "Pale", "Petite", "Poor", "Powerful", "Quaint", "Rare", "Reassuring", "Remarkable", "Rotten", "Rough", "Ruined", "Rustic", "Scary", "Simple", "Small", "Smelly", "Smooth", "Soft", "Strong", "Tranquil", "Ugly", "Valuable", "Warlike", "Warm", "Watery", "Weak", "Young"]
+
+
+let first
+let second
+
+function Generator(table) {
+
+  if(table == "action") {
+    first = action1
+    second = action2
+  } else if(table == "description") {
+    first = description1
+    second = description2
+  }
+  
+  return (
+    first[Math.floor(Math.random() * first.length)] + " / " + second[Math.floor(Math.random() * second.length)]
+  );
+}
+
+export default Generator;
