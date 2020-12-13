811a3d4bc02cf1d109af0b0e542e51a535c0fb24
Robby Hunt <robby@Robbys-MacBook-Pro.local>
2020-12-13 00:30:55 +0000

    added missing dice

--- src/util/DiceRoll.js
+++ src/util/DiceRoll.js
@@ -1,8 +1,11 @@
 import AddToLog from "./AddToLog";
+import DiceSound from "./DiceSound"
 
 function RollDice(event) {   
 
   const result = ` > ${event.target.id} result: ${Math.floor(Math.random() * event.target.id.substring(1, 3)) + 1}`;
+
+  DiceSound()
   
   return(
     AddToLog() + AddToLog(result)
